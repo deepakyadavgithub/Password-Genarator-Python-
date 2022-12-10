01ab75c2cd25438ed5d3b83ab3a753ca8c6eeb9b
Deepak yadav <112802026+deepakyadavgithub@users.noreply.github.com>
2022-12-10 15:53:14 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -3,8 +3,12 @@ lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NUMBER = "1234567890"
 Symbol = "!@#$%^&*"
-
-all = lower + upper + NUMBER + Symbol
-length = 6
-password = "".join(random.sample(all,length))
-print("The randomised Password You Generated is :",password)
+a="yes"
+while a="yes":
+  all = lower + upper + NUMBER + Symbol
+  length = 6
+  password = "".join(random.sample(all,length))
+  print("The randomised Password You Generated is :",password)
+  a=input("do you want to generate more yes/no" :)
+  if a=="no":
+    break
