2f79cb786e54c3632cc59693a7e1cdebd6f1c91c
NikhilBhati383 <120075788+NikhilBhati383@users.noreply.github.com>
2022-12-10 16:57:53 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -5,7 +5,8 @@ NUMBER = "1234567890"
 Symbol = "!@#$%^&*"
 a="yes"
 while a="yes":
-  all = lower + upper + NUMBER + Symbol
+  all = lower + upper + NUMBER + Symbol #this helps to make a strong password  because strorng password is a sum of  all special character
+ 
   length = 6
   password = "".join(random.sample(all,length))
   print("The randomised Password You Generated is :",password)
